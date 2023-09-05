cd69312990b96a483e8d127214bcd33ce2e667b9
maulana <maulana@drip.id>
2023-09-05 17:14:49 +0800

    JSON

---
+++ src/pages/usecase-api.ts
@@ -0,0 +1,21 @@
+import { getCollection } from "astro:content";
+ 
+const projects = (await getCollection("usecase")).sort((a, b) => a.data.id - b.data.id);
+ 
+
+  
+  import type { APIRoute } from 'astro'; 
+export const get: APIRoute = async function get() {
+  try {
+   return   new Response(JSON.stringify(projects), {
+    status: 200,
+    headers: { 'content-type': `application/json` },
+  });
+
+  } catch (error: unknown) {
+    return new Response(`Something went wrong in json route!: ${error as string}`, {
+      status: 501,
+      statusText: 'Server error',
+    });
+  }
+};
